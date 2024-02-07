5737343524e2d603a282b20037cb12e86b110f90
Carlstong <carlstongrefalde@gmail.com>
2024-02-08 00:19:41 +0800

    fixed bugs sanity

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-declare module '@sanity/block-content-to-react' {
+    declare module '@sanity/block-content-to-react' {
     // Add any necessary type information here
     const BlockContent: any; // You might need to replace 'any' with the actual type
   
